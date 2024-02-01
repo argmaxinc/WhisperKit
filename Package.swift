b4c1dcf9e7b2934f4300e07269edebbd146fe20a
ZachNagengast <znagengast@gmail.com>
2024-02-01 08:02:29 -0800

    Update swift-transformers dep to previous commit

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["WhisperKitCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/huggingface/swift-transformers.git", branch: "main"),
+        .package(url: "https://github.com/huggingface/swift-transformers.git", branch: "5e4031e"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
     targets: [
