d08fb1b67567d92b481adfedf2a6b42d51ab2c58
Finn Voorhees <finnvoorhees@gmail.com>
2024-03-08 00:27:34 +0000

    Expose downloadBase in WhisperKit init (#57)

--- Sources/WhisperKit/Core/WhisperKit.swift
+++ Sources/WhisperKit/Core/WhisperKit.swift
@@ -49,6 +49,7 @@ public class WhisperKit: Transcriber {
 
     public init(
         model: String? = nil,
+        downloadBase: URL? = nil,
         modelRepo: String? = nil,
         modelFolder: String? = nil,
         computeOptions: ModelComputeOptions? = nil,
@@ -74,7 +75,7 @@ public class WhisperKit: Transcriber {
         Logging.shared.logLevel = verbose ? logLevel : .none
         currentTimings = TranscriptionTimings()
 
-        try await setupModels(model: model, modelRepo: modelRepo, modelFolder: modelFolder, download: download)
+        try await setupModels(model: model, downloadBase: downloadBase, modelRepo: modelRepo, modelFolder: modelFolder, download: download)
 
         if let prewarm = prewarm, prewarm {
             Logging.info("Prewarming models...")
@@ -179,7 +180,7 @@ public class WhisperKit: Transcriber {
     }
 
     /// Sets up the model folder either from a local path or by downloading from a repository.
-    public func setupModels(model: String?, modelRepo: String?, modelFolder: String?, download: Bool) async throws {
+    public func setupModels(model: String?, downloadBase: URL? = nil, modelRepo: String?, modelFolder: String?, download: Bool) async throws {
         // Determine the model variant to use
         let modelVariant = model ?? WhisperKit.recommendedModels().default
 
@@ -189,7 +190,7 @@ public class WhisperKit: Transcriber {
         } else if download {
             let repo = modelRepo ?? "argmaxinc/whisperkit-coreml"
             do {
-                let hubModelFolder = try await Self.download(variant: modelVariant, from: repo)
+                let hubModelFolder = try await Self.download(variant: modelVariant, downloadBase: downloadBase, from: repo)
                 self.modelFolder = hubModelFolder!
             } catch {
                 // Handle errors related to model downloading
