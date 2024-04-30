69afbebf8cbcfa0f5e5908d906ac7a97baca6424
Jan Krukowski <jan.krukowski@geneva.com>
2024-04-30 11:01:52 +0200

    conditional watchos compilation

--- Package.swift
+++ Package.swift
@@ -40,11 +40,16 @@ let package = Package(
             name: "WhisperKitMLX",
             dependencies: [
                 "WhisperKit",
-                .product(name: "MLX", package: "mlx-swift"),
-                .product(name: "MLXRandom", package: "mlx-swift"),
-                .product(name: "MLXNN", package: "mlx-swift"),
-                .product(name: "MLXOptimizers", package: "mlx-swift"),
-                .product(name: "MLXFFT", package: "mlx-swift")
+                .product(
+                    name: "MLX",
+                    package: "mlx-swift",
+                    condition: .when(platforms: [.iOS, .macOS, .visionOS])
+                ),
+                .product(
+                    name: "MLXFFT",
+                    package: "mlx-swift",
+                    condition: .when(platforms: [.iOS, .macOS, .visionOS])
+                )
             ],
             path: "Sources/WhisperKit/MLX",
             resources: [
