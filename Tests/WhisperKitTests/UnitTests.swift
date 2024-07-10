0a1b066b149471204336aa8b1c6b1acfe5ca5d22
ZachNagengast <znagengast@gmail.com>
2024-07-10 15:38:26 -0700

    Add bigger range for early stopping test

--- Tests/WhisperKitTests/UnitTests.swift
+++ Tests/WhisperKitTests/UnitTests.swift
@@ -391,7 +391,7 @@ final class UnitTests: XCTestCase {
         let decodingTimePerTokenWithWait = resultWithWait.timings.decodingLoop / Double(tokenCountWithWait)
 
         // Assert that the decoding predictions per token are not slower with the waiting
-        XCTAssertEqual(decodingTimePerTokenWithWait, decodingTimePerToken, accuracy: decodingTimePerToken * 0.75, "Decoding predictions per token should not be significantly slower with waiting")
+        XCTAssertEqual(decodingTimePerTokenWithWait, decodingTimePerToken, accuracy: decodingTimePerToken, "Decoding predictions per token should not be significantly slower with waiting")
 
         // Assert that more tokens are returned in the callback with waiting
         XCTAssertGreaterThan(tokenCountWithWait, tokenCount, "More tokens should be returned in the callback with waiting")
